af674fe50bc4ed258881e061fbd81fa507991324
jordibernadlusilla <jordibernadlusilla@users.noreply.github.com>
2014-07-14 19:03:25 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,17 @@
 ## functions do
 
 ## Create a list that represents a matrix 'x' jointly
-## with the inverse of 'x'.
+## with the inverse of 'x'. 
+##Arguments: 'x' a matrix
+##Returns: a list 'l' of four functions 
+##        - l&set(x): change the matrix
+##        - l&get(): returns the matrix
+##        - l&setinverse(inv): associated to x a matrix inv that it would be the inverse of x
+##        - l&getinverse(): return the associated matrix to x that it could be the inverse of x
 
 
 makeCacheMatrix <- function(x = matrix()) {
- 	  inv <- NULL
+ 	inv <- NULL
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
@@ -22,10 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Return a matrix that is the inverse of the "CacheMatrix" 'x' created
 ## by the function makeCacheMatrix
+## Arguments: 'x' a object returned by function makeCacheMatrix
+##            ... another argumentes for function solve(m,...)
+## Returns: the inverse associated to 'x'.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-	  inv <- x$getinverse()
+	inv <- x$getinverse()
         if(!is.null(inv)) {
                 #message("getting cached data")
                 return(inv)
